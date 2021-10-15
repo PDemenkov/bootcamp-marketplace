a80576697d5492d8b5699a594a2be7fa10208afa
Деменков Павел Сергеевич <PSDemenkov@sberbank.ru>
2021-10-15 10:01:58 +0300

    Jenkins1

--- src/test/java/com/sbt/bcamp/marketplace/HealthCheckControllerTest.java
+++ src/test/java/com/sbt/bcamp/marketplace/HealthCheckControllerTest.java
@@ -1,13 +1,20 @@
 package com.sbt.bcamp.marketplace;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,6 +22,9 @@ public class HealthCheckControllerTest {
 
     @Autowired
     HealthCheckController controller;
+
+//    @MockBean
+//    InfoProcAndMem info;
 
     @Test
     public void shouldReturnOK() {
@@ -25,3 +35,9 @@ public class HealthCheckControllerTest {
         Assert.assertEquals("Method check() always return OK", "OK", actual);
     }
 }
+//    @Test
+//    public void infoTest() {
+//        controller.info();
+//        verify(info).getMem();
+//        verify(info).getProc();
+//}
--- src/test/java/com/sbt/bcamp/marketplace/HealthCheckWebLayerTest.java
+++ src/test/java/com/sbt/bcamp/marketplace/HealthCheckWebLayerTest.java
@@ -1,15 +1,18 @@
   package com.sbt.bcamp.marketplace;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,4 +30,24 @@ public class HealthCheckWebLayerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(is("OK")));
     }
+//    @Test
+//    public void shouldReturnInfo() throws Exception {
+//        mockMvc.perform(get("/health/info")).andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(any(String.class)));
+//    }
+    @Test
+    public void testGetNameAndSurname() throws Exception {
+        String s = "{\"id\":3603,\"login\":\"19551447\",\"firstName\":\"Pavel\",\"lastName\":\"D.\",\"birthDate\":null}";
+        mockMvc.perform(get("/customers/19551447"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(is(s)));
+    }
+//    @Test
+//    public void testAddOrder() throws Exception{
+//        OrderDTO order = new OrderDTO(1,"test","test",1,true,1);
+//        CustomerController customerController = new CustomerController();
+//        Long l = customerController.addOrder(order);
+//        Assert.assertNotNull(l);
+//    }
 }
